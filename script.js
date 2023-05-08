f3f94f717240ea2f6b212fe9f168f714e825f290
Cylon-Skin-Job <105960223+Cylon-Skin-Job@users.noreply.github.com>
2023-05-07 23:03:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3898,7 +3898,7 @@ function cancelButtonNewCustomer() {
 // BBB Hash Change
 
 window.addEventListener('load', function () {
-    alert("Version 05.07.2023.LATE");
+    alert("Version 05.07.2023.BUTTON");
     buildOverview()
     location.hash = "#overview";
     siteHash = "#overview";
